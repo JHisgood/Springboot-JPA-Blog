e26b33b5ad6b669c6a8699500ced15470c34d824
JHisgood <lsj9079@gmail.com>
2022-07-28 13:59:53 +0900

    개발환경 설정

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.cos.blog.model.User;
 
@@ -7,13 +9,18 @@ import com.cos.blog.model.User;
 // 자동으로 bean 등록이 된다.
 // @Repository	// 생략 가능하다.
 public interface UserRepository extends JpaRepository<User, Integer> {
-	//	JPA Naming 쿼리
-	// SELECT * FROM user WHERE username = ?1 AND password = ?2;
-	User findByUsernameAndPassword(String username, String password);
-
-	// 둘중 하나
-	/*
-	 * @Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2",
-	 * nativeQuery = true) User login(String username, String password);
-	 */
+	// SELECT * FROM user WHERE username = 1?;
+	Optional<User> findByUsername(String username);
 }
+
+
+
+//JPA Naming 쿼리
+// SELECT * FROM user WHERE username = ?1 AND password = ?2;
+// User findByUsernameAndPassword(String username, String password);
+
+// 둘중 하나
+/*
+ * @Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2",
+ * nativeQuery = true) User login(String username, String password);
+ */
